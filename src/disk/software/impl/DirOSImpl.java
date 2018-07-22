c58dec5f5cfaf9bccd08826820b86b052c29d432
BillKiller <747441355@qq.com>
2018-07-23 01:05:04 +0800

    简单得文件夹得新建和查询目录下所有文件目录

--- src/disk/software/impl/DirOSImpl.java
+++ src/disk/software/impl/DirOSImpl.java
@@ -1,23 +1,54 @@
 package disk.software.impl;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import disk.hardware.FileStruct;
 import disk.software.DirOS;
+import disk.software.DiskManager;
+import myUtil.Number;
 
 public class DirOSImpl implements DirOS {
-
+	private DiskManager diskManagerImpl;
 	@Override
-	public boolean md(String path) {
+	public int md(int bnum,String path,int attribute) {
 		// TODO Auto-generated method stub
-		return false;
+		// TODO Auto-generated method stub
+				if(diskManagerImpl.getFreeStructPos(bnum)==-1)return -1;
+				if(diskManagerImpl.getFreeBlockNum()==0)return -1;
+				//满足磁盘块和目录项都足够的条件
+				
+				int pos=diskManagerImpl.getFreeBlockPos();
+				
+				FileStruct fileStruct = new FileStruct();
+				fileStruct.setName(path);
+				fileStruct.setType(" ");
+				fileStruct.setFileAttribute(Number.intToByte(attribute));
+				fileStruct.setStartPos(Number.intToByte(pos));
+				fileStruct.setFileLength((short)0);
+				
+				int pnum = diskManagerImpl.getFreeStructPos(bnum);
+				
+				diskManagerImpl.addStruct(bnum, pnum, fileStruct);
+				diskManagerImpl.storeFile(pos,null);
+				return pos;
 	}
 
 	@Override
-	public boolean dir(String path) {
+	public FileStruct[] dir(int bnum,String path) {
 		// TODO Auto-generated method stub
-		return false;
+		List<FileStruct>list=new ArrayList<FileStruct>();
+		for(int i=0;i<8;i+=8) {
+			FileStruct temp = diskManagerImpl.getFileStruct(bnum, i);
+			if(temp==null)break;
+			list.add(temp);
+		}
+		return (FileStruct[])list.toArray();
 	}
-
+		
 	@Override
-	public boolean rd(String path) {
+	public boolean rd(int bnum,String path) {
 		// TODO Auto-generated method stub
 		return false;
 	}
