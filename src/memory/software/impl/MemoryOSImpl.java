26167aa8bb01dc28a1cb50f20eb9efa0607f2d2a
A_Yu <498870048@qq.com>
2018-07-27 19:42:40 +0800

    实现内存分配和内存回收
    实现内存分配（首次适配），
    对于进程，分配内存时调用allocationForProcess(byte[] data)；回收内存时调用collection(int pid)
    对于文件，分配内存时调用allocationForFile(byte[] data, OpenFileItem openFileItem)，回收内存时调用collection(OpenFileItem openFileItem)

--- src/memory/software/impl/MemoryOSImpl.java
+++ src/memory/software/impl/MemoryOSImpl.java
@@ -2,54 +2,159 @@ package memory.software.impl;
 
 import java.util.LinkedList;
 
+import org.junit.Test;
+
 import memory.hardware.MemoryTable;
+import memory.hardware.MyMemory;
+import memory.hardware.OpenFileItem;
 import memory.hardware.PCB;
 import memory.hardware.Zone;
 import memory.software.MemoryOS;
 import myUtil.Number;
+
 /**
  * 软件实现内存管理类
+ * 
  * @author suisui
  *
  */
 public class MemoryOSImpl implements MemoryOS {
-	
-	private MemoryTable memoryTable;//内存分配表
-	private LinkedList<Zone> freeZones;//空闲区链表
+
+	private MyMemory memory;
+	private MemoryTable memoryTable;// 内存分配表
+	private PCB[] pcbs;
+	private LinkedList<Zone> freeZones;// 空闲区链表
 
 	public MemoryOSImpl() {
-		this.memoryTable = new MemoryTable();
+		// 初始化
+		this.memory = new MyMemory();
+		this.memoryTable = memory.getSystemArea().getMemoryTable();
+		this.pcbs = memory.getSystemArea().getPcb();
 		this.freeZones = new LinkedList<>();
-		freeZones.add(new Zone(0, Number.sizeOfUserArea));
+
+		freeZones.add(new Zone(0, Number.sizeOfUserArea, true));
 	}
-	
-	/**
-	 * 内存分配
-	 * 
-	 * @param size 申请内存大小
-	 */
-	public boolean allocation(byte[] data) {
+
+	@Override
+	public boolean allocationForFile(byte[] data, OpenFileItem openFileItem) {
 		int size = data.length;
-		firstFit(size);// 默认首次适配
-		return true;
+		if (firstFit(size) != -1) {// 默认首次适配
+			
+			memory.getSystemArea().getOpenFileTable().getOpenFileTable().add(openFileItem);
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean allocationForProcess(byte[] data) {
+		int size = data.length;
+		if (firstFit(size) != -1) {// 默认首次适配
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean collection(long pid) {
+		PCB tempPcb;
+
+		for (PCB pcb : pcbs) {
+			if (pcb.getPid() == pid) {
+				tempPcb = pcb;
+				break;
+			}
+		}
+		for (Zone zone : memoryTable) {
+
+		}
+		return false;
+	}
+
+	@Override
+	public boolean collection(OpenFileItem openFileItem) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public PCB[] getPCB() {
+		return pcbs;
+	}
+
+	@Override
+	public byte[] getUserArea() {
+		return memory.getUserArea().getMemData();
 	}
 
 	/**
-	 * 内存回收
-	 * 
-	 * @param pid 释放进程标志符为pid进程所占用的内存
+	 * 显示当前空闲块链表，测试用
 	 */
-	public void collection(long pid) {
-		
+	public void showFreeZone() {
+		System.out.println("showFreeZone:");
+		for (int j = 0; j < freeZones.size(); j++) {
+			System.out.println("j=" + j + " " + freeZones.get(j));
+		}
+		System.out.println();
 	}
 
+	/**
+	 * 显示当前内存分配表，测试用
+	 */
+	public void showMemoryTable() {
+		System.out.println("showMemoryTable:");
+		for (int j = 0; j < memoryTable.size(); j++) {
+			System.out.println("j=" + j + " " + memoryTable.get(j));
+		}
+		System.out.println();
+	}
+
+	private void saveData(byte[] data) {
+		int head=firstFit(data.length);
+		for(int i=head;i<data.length;i++) {
+			
+		}
+	}
 	/**
 	 * 首次适配
 	 * 
 	 * @param size 申请内存大小
+	 * @return 分配成功返回分配的内存块起址，失败返回-1
 	 */
-	private void firstFit(int size) {
-		
+	private int firstFit(int size) {
+		int i = 0;
+		for (Zone zone : freeZones) {
+
+			if (size <= zone.getZoneSize()) {
+//				System.out.println("Zone:"+zone);
+				int remainSize = zone.getZoneSize() - size;// 剩余空闲区大小
+				int newHead = zone.getHead() + size;// 空闲区的新起址
+				Zone busyZone = new Zone(zone.getHead(), size, false);// 分配忙内存空间
+				
+				if (remainSize != 0) {
+					Zone freeZone = new Zone(newHead, remainSize, true);// 新空闲块
+					// 更新空闲区链表
+					freeZones.remove(zone);// 删除旧空闲区
+					freeZones.add(i, freeZone);// 增加新空闲区
+
+					// 更新内存分配表
+					memoryTable.remove(zone);// 删除旧内存块
+					memoryTable.add(busyZone);// 增加忙内存块
+					memoryTable.add(freeZone);// 增加空闲内存块
+					showMemoryTable();
+				}else {//remainSize == 0
+					// 更新空闲区链表
+					freeZones.remove(zone);// 删除旧空闲区
+					memoryTable.remove(zone);// 删除旧内存块
+					memoryTable.add(busyZone);// 增加忙内存块
+					showMemoryTable();
+				}
+				return zone.getHead();
+			} else {
+				i++;
+			}
+		}
+		return -1;
 	}
 
 	/**
@@ -67,7 +172,7 @@ public class MemoryOSImpl implements MemoryOS {
 	 * @param size
 	 */
 	private void bestFit(int size) {
-		
+
 	}
 
 	/**
@@ -76,20 +181,6 @@ public class MemoryOSImpl implements MemoryOS {
 	 * @param size
 	 */
 	private void worstFit(int size) {
-
-	}
-
-	@Override
-	public PCB[] getPCB() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void add(int size) {
-
-	}
-
-	public void remove() {
 
 	}
 }
